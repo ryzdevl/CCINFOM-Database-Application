cde22aebaec7b49f663a6cc7985da2723c68fd3e
ryzdevl <ryan_malapitan@dlsu.edu.ph>
2025-11-19 18:36:03 +0800

    fix: name format for sql tables

--- database/DatabaseConnection.java
+++ database/DatabaseConnection.java
@@ -3,9 +3,9 @@ package database;
 import java.sql.*;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/beach_resort";
+    private static final String URL = "jdbc:mysql://localhost:3306/CCINFOM-S27-06-DBCREATION";
     private static final String USER = "root"; // Change to your MySQL username
-    private static final String PASSWORD = "<User Password>"; // Change to your MySQL password (Important!)
+    private static final String PASSWORD = "1"; // Change to your MySQL password (Important!)
 
     public static Connection getConnection() throws SQLException {
         try {
